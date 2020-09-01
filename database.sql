27927d32ff600dd964695dca48aad16507b1f783
Darcy Knox <darcy@cloudcannon.com>
2020-09-01 12:50:54 +1200

    update database for testing

--- database.sql
+++ database.sql
@@ -1,8 +1,14 @@
 CREATE TABLE players (
   name varchar(50) NOT NULL,
   position varchar(10),
+  rating tinyint;
   PRIMARY KEY (name)
 );
 
-INSERT INTO players VALUES ('Marty Barnsley','Guard');
-INSERT INTO players VALUES ('Jonas Atkinson','Forward');
+INSERT INTO players VALUES ('Marty Barnsley','Guard',30);
+INSERT INTO players VALUES ('Jonas Atkinson','Forward',45);
+INSERT INTO players VALUES ('LeBron James','Forward',98);
+INSERT INTO players VALUES ('Michael Jordan','Guard',99);
+INSERT INTO players VALUES ('Kareem Abdul-Jabbar','Center',95);
+INSERT INTO players VALUES ('John Stockton','Guard',45);
+INSERT INTO players VALUES ('Larry Bird','Forward',93);
